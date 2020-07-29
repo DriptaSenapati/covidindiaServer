fc7a825dde1712e3300106f35b3e300af8608057
DriptaSenapati <driptasenapati97@gmail.com>
2020-07-29 17:22:52 +0530

    final

--- static/covdata.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $("li:eq(6)").addClass("active");
-  var menu = document.querySelector(".hamburger-menu");
-  var bar = document.querySelector(".nav-bar");
-  menu.addEventListener("click", () => {
-    bar.classList.toggle("change");
-  });
-});
